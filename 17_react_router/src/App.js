11f2243a9065deacf0c74372f6ed24ca0cfd58f9
blgn9958 <blgn9958@gmail.com>
2024-04-19 10:26:27 +0900

    react router

---
+++ 17_react_router/src/App.js
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Test from "./components/Test";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>routing!</h1>
+
+      {/* <Routes>
+        <Route path="/test" element={<Test />} />
+      </Routes> */}
+    </div>
+  );
+}
+
+export default App;
